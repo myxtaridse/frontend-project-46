e801a69296cc6b6a433478e382f7710e00a94714
Ksenia Durkina <sigismund1old@gmail.com>
2026-05-12 11:49:21 +0300

    feat: add new option -f for output format

--- gendiff.js
+++ gendiff.js
@@ -3,7 +3,10 @@ import { program } from 'commander'
 
 program
   .name('gendiff')
-  .description('A tool for dealing with differences')
+  .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .option('-f, --format [type]', ' output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
 
 program.parse()
