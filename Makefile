1ee00c40a95a75eff20ff54023280de8147a9628
Ksenia Durkina <sigismund1old@gmail.com>
2026-05-12 11:34:35 +0300

    feat: add executing a command via the command line

--- Makefile
+++ Makefile
@@ -1,6 +1,13 @@
 install:
 	npm ci
 
+# Настройка команд в системе для разработки
+link:
+	npm link
+
+# Удобная обертка для первого запуска
+setup: install link
+
 lint:
 	npx eslint .
 
--- gendiff.js
+++ gendiff.js
@@ -1,8 +1,9 @@
-import { program } from "commander"
+#!/usr/bin/env node
+import { program } from 'commander'
 
 program
-    .name("gendiff")
-    .description("A tool for dealing with differences")
-    .version("1.0.0")
+  .name('gendiff')
+  .description('A tool for dealing with differences')
+  .version('1.0.0')
 
 program.parse()
